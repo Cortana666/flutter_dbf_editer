781834994eec408f9fc950706d353955719631e6
Cortana <jian635446559@gmail.com>
2023-02-14 16:00:58 +0800

    增加右键打开方式

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,9 +2,22 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
+  open var currentFile: String?
+
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
+
+    let channel = FlutterMethodChannel(name: "myChannel", binaryMessenger: flutterViewController.engine.binaryMessenger)
+    channel.setMethodCallHandler({
+      (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      if (call.method == "getCurrentFile") {
+        result(self.currentFile)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    })
+
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
 
